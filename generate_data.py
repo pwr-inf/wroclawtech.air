12b9dfdebf7559c058883255871cbeb06bf37e0b
pawelptak <pawelptak>
2023-04-18 16:01:45 +0200

    add file for generating experimental data

---
+++ generate_data.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import os
+pd.options.plotting.backend = "plotly"
+import plotly.io as pio
+pio.renderers.default = "browser"
+import numpy as np
+import argparse
+
+# remove values greater than X
+def remove_outliers(df, column: str):
+    historical_max = 352.6 # max historical data from https://powietrze.gios.gov.pl/pjp/archives/downloadFile/423
+    for column_name in df.columns:
+        if column_name.endswith(column):
+            df = df[df[column_name] <= historical_max]
+    return df
+
+
+def rename_columns(df, suffix):
+    new_column_names = dict()
+    for column_name in df.columns:
+        if column_name != 'timestamp':
+            new_column_names[column_name] = f"{suffix}.{column_name}"
+    # print(new_column_names)
+    df.rename(columns=new_column_names, inplace=True)
+
+
+def filter_df_on_dates(df, dates: dict):
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    dfs = []
+    for year, months in dates.items():
+        months_int = [int(x) for x in months]
+        mask = (df['timestamp'].dt.year == int(year)) & (df['timestamp'].dt.month.isin(months_int))
+        year_df = df.loc[mask]
+        dfs.append(year_df)
+    return pd.concat(dfs)
+
+
+def generate_dataset(window: str, input_dir: str, output_dir: str, year: str):
+    dates_dict = dict()
+
+    readings_features = ['PM2.5[calibrated]', 'Temperature[OUT, C]', 'RelativeHumidity[OUT, %]', 'Pressure [hPa]']
+
+    if year == '2019':
+        dates_dict['2018'] = ['12']
+        months = [str(i).zfill(2) for i in range(1, 11)]
+        dates_dict['2019'] = months
+    elif year == '2020':
+        dates_dict['2019'] = ['11', '12']
+        months = [str(i).zfill(2) for i in range(1, 11)]
+        dates_dict['2020'] = months
+    elif year == '2021':
+        dates_dict['2020'] = ['11', '12']
+        months = [str(i).zfill(2) for i in range(1, 11)]
+        dates_dict['2021'] = months
+    elif year == '2022':
+        dates_dict['2021'] = ['11', '12']
+        months = [str(i).zfill(2) for i in range(1, 7)]
+        dates_dict['2022'] = months
+
+    # sensor_ids = [f'10{str(x).zfill(2)}' for x in range(1, 21)]
+    sensor_ids = [f'10{str(x).zfill(2)}' for x in range(1, 6)] # for testing
+
+    all_years_dfs = []
+
+    for year, months in dates_dict.items():
+        for i, month in enumerate(months):
+            month_dfs = []
+            print(f'Processing: {year}-{month}')
+            for sensor_id in sensor_ids:
+                for file_name in os.listdir(input_dir):
+                    fname_sensor_id = file_name[:file_name.find('_')]
+                    fname_year = file_name[file_name.find('_') + 1:file_name.rfind('_')]
+                    fname_month = file_name[file_name.rfind('_') + 1: file_name.find('.')]
+                    if fname_month == month and fname_year == year and fname_sensor_id == sensor_id:
+                        m_df = pd.read_feather(os.path.join(input_dir, file_name), columns=readings_features + ['timestamp']).set_index('timestamp')
+                        m_df[m_df.select_dtypes(np.float64).columns] = m_df.select_dtypes(np.float64).astype(np.float16)
+                        m_df = m_df.resample(window).mean() # this fixed most of the memory problems
+                        # m_df = remove_outliers(m_df, column='PM2.5[calibrated]') # removes rows where extreme data occured
+                        rename_columns(m_df, sensor_id)
+                        month_dfs.append(m_df)
+                        del(m_df)
+
+            if len(month_dfs):
+                all_columns_df = month_dfs[0].join(month_dfs[1:])
+                all_years_dfs.append(all_columns_df)
+                del(all_columns_df)
+
+    all_years_df = pd.concat(all_years_dfs)
+
+    # all_years_df = all_years_df.resample('15min').mean().ffill()
+    all_years_df = remove_outliers(all_years_df, column='PM2.5[calibrated]') # removes rows where extreme data occured
+    # fig = all_years_df.plot()
+    # fig.show()
+
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+
+    output_file = os.path.join(output_dir, f'{year}.csv')
+    all_years_df.to_csv(output_file)
+    print(f"Done. Output files saved in: {output_dir}.")
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("window", help="averaging window")
+parser.add_argument("input_dir", help="path do the raw .feather files directory")
+parser.add_argument("output_dir", help="path do the output .csv files directory")
+args = parser.parse_args()
+
+years = ['2019', '2020', '2021', '2022']
+
+for year in years:
+    # possible windows: https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
+    generate_dataset(window=args.window, input_dir=args.input_dir, output_dir=args.output_dir, year=year)
